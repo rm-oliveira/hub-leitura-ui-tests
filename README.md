7311c6f3ad39f1f7ff633840542df64b4b0482ac
Rayane Oliveira <rayanemoliveira22@gmail.com>
2026-02-16 19:10:20 -0300

    add Readme e automacao do contato

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# hub-leitura-ui-tests
+# Hub de Leitura - Ui Tests
+
+Este repositório contém testes automatizados de interface (UI) desenvolvidos com **Cypress** para o sistema **Hub de Leitura**, com foco em testes end-to-end (E2E).
+
+O projeto foi desenvolvido para fins educacionais e de portfólio, aplicando boas práticas de automação de testes.
+
+## Sistema Testado
+
+Os testes deste projeto são executados sobre a aplicação **Hub de Leitura**, disponibilizada pela EBAC.
+
+Repositório da aplicação:
+
+https://github.com/EBAC-QE/hub-de-leitura-integrado
+
+⚠️ Antes de rodar os testes, certifique-se de que o servidor da aplicação esteja rodando localmente conforme as instruções do README oficial.
+
+## Execução dos testes (UI)
+
+### Pré-requisitos
+
+- Node.js (versão LTS)
+- npm
+- Editor de código (Visual Studio Code recomendado)
+- Aplicação **Hub de Leitura** rodando localmente
+
+### Instalação do projeto de testes
+
+Clone este repositório e instale as dependências:
+```bash
+git clone https://github.com/rm-oliveira/hub-leitura-ui-tests.git
+
+cd hub-leitura-ui-tests
+
+npm install
+````
+
+### Executando os testes
+
+Abrir o Cypress em modo interativo:
+```bash
+npx cypress open
+```
+Executar os testes em modo headless:
+```bash
+npx cypress run
+```
+### Configuração da aplicação
+
+A URL da aplicação testada pode ser ajustada no arquivo cypress.config.js.
+
+http://localhost:3000
+
+## Tecnologias utilizadas
+
+- Cypress
+- JavaScript
+- Node.js
+- Git & GitHub
---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,51 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('index.html')
+  });
+
+  it('Deve preencher formulário de contato com sucesso', () => {
+    cy.get('#name').type('Rayane')
+    cy.get('#email').type('rayane@teste.com')
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Contato enviado com sucesso!')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o nome', () => {
+    cy.get('#name').clear()
+    cy.get('#email').type('rayane@teste.com')
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preenchero email', () => {
+    cy.get('#name').type('Rayane')
+    cy.get('#email').clear()
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+    cy.get('#name').type('Rayane')
+    cy.get('#email').type('rayane@teste.com')
+    //cy.get('#subject').select('Sugestões')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+    cy.get('#name').type('Rayane')
+    cy.get('#email').type('rayane@teste.com')
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').clear()
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+  })
+})
